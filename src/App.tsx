dbd9a6444982f46e880008931405ce9f8f2f5e23
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-09-25 09:35:22 -0300

    feat: form and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { StyledContainer, StyledForm, StyledHeader, StyledInput, StyledLabel, StyledSubmitButton } from "./styles";
+import GlobalStyles from "./styles/GlobalStyles";
+import axios from "axios";
 
 function App() {
+
+  function sendData(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log('Mensagem de aviso mesmo!')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles/>
+      <StyledContainer>
+        <StyledHeader>Calculadora de Imóveis</StyledHeader>
+        Informe os dados para prever o preço do imóvel!
+        <StyledForm onSubmit={sendData}>
+          <StyledLabel htmlFor="room">Quartos</StyledLabel>
+          <StyledInput type="text" id="Quartos" placeholder="Insert text in here" />
+          <StyledLabel htmlFor="bathroom">Banheiros</StyledLabel>
+          <StyledInput type="text" placeholder="Insert text in here" />
+          <StyledLabel htmlFor="garage">Vagas de garagem</StyledLabel>
+          <StyledInput type="text" placeholder="Insert text in here" />
+          <StyledLabel htmlFor="bairro">Bairro</StyledLabel> {/* TODO COLOCAR SELECT */}
+          <StyledInput type="text" placeholder="Insert text in here" />
+          <StyledLabel htmlFor="zona">Zona</StyledLabel> {/* TODO COLOCAR SELECT */}
+          <StyledInput type="text" placeholder="Insert text in here" />
+          <StyledLabel htmlFor="area">Área (m²)</StyledLabel>
+          <StyledInput type="text" placeholder="Insert text in here" />
+          <StyledLabel htmlFor="area">Área (m²)</StyledLabel>
+          <StyledInput type="text" placeholder="Insert text in here" />
+          <StyledSubmitButton type="submit">Clique aqui!</StyledSubmitButton>
+        </StyledForm>
+      </StyledContainer>
+    </>
   );
 }
 
 export default App;
+
